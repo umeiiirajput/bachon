f9fb930cd80d2efd21f05da72c424eb7fc66e237
UME <51955264+umeiiirajput@users.noreply.github.com>
2019-08-08 12:05:41 +0500

    d

---
+++ KidsLearning/Cosmetics/download.php
@@ -0,0 +1,21 @@
+<?php
+include_once 'simple_html_dom.php';
+$html = file_get_html('http://www.kidsfront.com/words/cosmetics.html');
+
+// Find all images 
+//foreach($html->find('img') as $element) 
+//{
+       //echo "<a href='http://www.kidsfront.com/words/".$element->src."'>".$element->src."</a><br>";
+	//   file_put_contents("KidsBodyParts/"."http://www.kidsfront.com/words/".$element->src, fopen($img, 'r'));
+	   //file_put_contents("manual", file_get_contents("http://www.kidsfront.com/words/".$element->src));
+//}
+foreach($html->find('img') as $e){
+    $img = "http://www.kidsfront.com/words/".$e->src;
+  //echo "<a href='".$e->src ."'>.$e->src .</a><br>";
+	$myArray = explode('/', $img);
+		//echo $myArray['6']."<br>";
+		//print_r($myArray);
+		file_put_contents("img/".$myArray['6'], fopen($img, 'r'));
+		//echo $img."<br>";
+}
+?>
